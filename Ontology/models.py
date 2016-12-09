b8fdf23c5b11f97bf3403900317c3a7b48531ece
guilhermenoronha <guilhermenoronha2001@gmail.com>
2016-12-09 20:56:43 -0200

    Added a form to /ontology page changed field date_added to uploaded_at

--- Ontology/models.py
+++ Ontology/models.py
@@ -3,13 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 from django.core.validators import MaxValueValidator
 from CustomUser.models import CustomUser 
+from django.core.exceptions import ValidationError 
 
-def index(request):
-    return render(request, 'index.html')
+def validate_file_extension(value):
+    if value.file.content_type != 'application/xml' and not value.name.endswith('.owl'):
+        raise ValidationError(u'File not supported.')
 
 class Ontology(models.Model):
     name = models.TextField(max_length=50)
-    size = models.PositiveIntegerField(validators=[MaxValueValidator(5120)])
-    date_added = models.DateTimeField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    file = models.FileField(default="")
+    file = models.FileField(default="", validators=[validate_file_extension])
--- Ontology/views.py
+++ Ontology/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import UploadOntologyForm
+from django.views.generic import CreateView
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        form = UploadOntologyForm(request.POST, request.FILES)
+        if form.is_valid():
+            ontology = form.save(commit=False)
+            ontology.name = request.FILES['file'].name
+            ontology.user = request.user
+            ontology.save()
+            return HttpResponseRedirect('/ontology/')
+    else:
+          form = UploadOntologyForm()
+    
+    return render(request, 'index.html', {'form': form})
+        
